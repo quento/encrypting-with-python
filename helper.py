92ec2bbec4cda946a6c5a93350731a14c68bde62
QuentO <qochieng@gmail.com>
2019-03-03 01:56:50 -0800

    Update helper.py
    tested reacCert() and ceasarCipher() functionality here.

--- helper.py
+++ helper.py
@@ -11,6 +11,32 @@ def ceasarCipher(plaintext, shift):
 
     return plaintext.translate(table)
 
+def readCert(cert,shift):
+    """ Check server certificate. If valid, return true. If not return false """
+    
+    server_cert = "I am Simple Server"
+
+    decipher_cert = ceasarCipher(cert, -shift)
+    
+    result = False
+    print("Server cert: ", server_cert, " Decipher Cert: ", decipher_cert)
+    if server_cert == decipher_cert:
+        result = True
+    else:
+        result = False
+
+    return result
+
+
+def main():
+    """ Test the helper functions """
+    server_cert = "I am Simple Server"
+    shift = 1
+    encoded_cert = ceasarCipher(server_cert,shift)
+    print(readCert(encoded_cert,shift))
+
+    print("Encoded Cert: ", ceasarCipher(server_cert,shift))
+    print("Dencoded Cert: ", ceasarCipher(encoded_cert,shift))
 
 if __name__ == '__main__':
     main()
