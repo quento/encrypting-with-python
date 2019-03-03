06952f5cc194037b2688b8fd9035255d510fb579
QuentO <qochieng@gmail.com>
2019-03-03 01:55:05 -0800

    Update client.py
    Made host and port parameters on class instantiation. Added checkCA() and checkWithCA() to handle second connection to CA server.

--- client.py
+++ client.py
@@ -3,9 +3,9 @@ import socket
 class SimpleClient:
     "Simple client that communicats with a socket server."
 
-    def __init__( self ):
-        self._server = '127.0.0.1'
-        self._port = 9500
+    def __init__( self, host = '127.0.0.1', port = 9500  ):
+        self._server = host
+        self._port = port
 
     def create_socket( self ):
         try:
@@ -26,10 +26,53 @@ class SimpleClient:
 
             result = sock.recv( 4096 )
 
-            print( 'Response received: ', result.decode() )
+            response_msg = result.decode()
+            print( 'Response received: ', response_msg )
+
+            # Check if response has Certificate marker in msg.
+            if response_msg.find("CA:") > -1:
+                response_cert = response_msg
+                # Verify Cert. with CA
+                print("Sending cert to CA server .....")
+                CA_status = self.checkWithCA('127.0.2.1', 9000, response_cert)
+
+                if CA_status == True:
+                    print("Certificate is valid. You may proceed")
+                    print("Recieved public key for CA server...")                    
+                else:
+                    print("Warning: Certificate is invalid!!!")
+                
         except Exception as err:    
             print("Connection Error:\n {0}".format(err))
 
+    def checkCA(self, cert):
+        status = False
+        try:
+            sock = self.create_socket()
+            sock.connect( (self._server, self._port) )            
+
+            print( "Sending CA cert for verification...." ) 
+
+            sock.sendall( cert.encode() )
+
+            result = sock.recv( 4096 )
+
+            response_msg = result.decode()
+            print( 'Response received: ', response_msg )
+
+            if response_msg != 'INVALID':
+                status = True
+            
+        except Exception as err:    
+            print("CheckCA() Connection Error:\n {0}".format(err))
+        
+        return status
+
+    def checkWithCA(self, host, port, cert):
+        """ A certificate has been received, Check with CA if it's valid """
+        ca_client = SimpleClient(host, port)
+        ca_response = ca_client.checkCA(cert)
+        return ca_response
 
 def display_helper(msg):
     print("****************** ", msg, "******************")
