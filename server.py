2048013913e4aab13d6eafd54bffd998a2fbf916
QuentO <qochieng@gmail.com>
2019-03-03 22:15:17 -0800

    Update server.py
    Added client_secret property. That can be used for communication during a session.

--- server.py
+++ server.py
@@ -5,11 +5,19 @@ from helper import simpleCipher
 class SimpleServer:
     """
     This class creates a simple socket server that listens on a specific port.
-    """    
+    """   
+
+    server_private_key = ""
+    _server_certificate = ""
+    _server_public_key = ""
+    _client_secret = ""
+
     def __init__( self, host = '127.0.0.1', port = 9500 ):        
         self._host = host
         self._port = port
-            
+        self._server_certificate = "CA: I am Simple Server Certificate"
+        self._server_public_key = "Server Public Key"
+                   
     def create_socket(self):        
         try:
             return socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
@@ -21,7 +29,7 @@ class SimpleServer:
         # Create socket
         sock = self.create_socket()
         print("Socket created...")
-
+        status = False
                 
         try:            
             with sock:
@@ -30,7 +38,7 @@ class SimpleServer:
                 print("Socket bound to ", self._host)
 
                 while True:
-                    sock.listen(5)
+                    sock.listen(15)
                     print("Server is listening on port ", self._port)
                     
                     conn, addr = sock.accept()
@@ -40,18 +48,32 @@ class SimpleServer:
                     with conn:
                         while True:
                             data = conn.recv( 1024 )
-                            if data == b'Hello':                        
-                                print( "Server received '{0}'".format(data.decode()) )
-                                print( "Server sending back - Certificate + Public Key" )
-                                # Put Cert + Public Key together and encrypt                                
-                                server_cert = simpleCipher( self.compileCertificate(),1,'e' )                               
-                                conn.sendall(server_cert.encode('utf-8'))                    
-                            else:
-                                print( "Server received '{0}'".format(data.decode()) )
-                                print( "Server sending back - 'Goodbye' response" )
-                                print()
-                                conn.sendall(b'Goodbye')  
-                                break  
+                            print( "length of data received " + str(len(data)) )
+                            if status == False:
+                                if data == b'Hello':                        
+                                    print( "Server received '{0}'".format(data.decode()) )
+                                    print( "Server sending back - Certificate + Public Key" )
+                                    # Put Cert + Public Key together and encrypt                                
+                                    server_cert = simpleCipher( self.compileCertificate(),1,'e' ) 
+                                    print("server_cert: " + server_cert)                              
+                                    conn.sendall(server_cert.encode('utf-8'))                                                    
+                                elif len(data) == 0:                             
+                                    conn.sendall(b'Goodbye')  
+                                    break 
+                                else:
+                                    print( "Server received '{0}'".format(data.decode()) )
+                                    print( "Key+Secret Decrypted = " + simpleCipher( data.decode(),1,'d') )
+                                    decrypt_secret = simpleCipher( data.decode(),1,'d')
+                                    self.setClientSecret(decrypt_secret)
+
+                                    print( "Server sending back - 'Ready to communicate' will use client secret " + self._client_secret )
+                                    print()
+                                    #Send response using secret
+                                    secret_response = simpleCipher('Read to commnicate using secret~' + self.getClientSecret(),1,'e')
+                                    conn.sendall(secret_response.encode()) 
+                                    status == True
+                            elif data.decode().find('VojrvfLfz') > -1:  # the word 'UniqueKey' encrypted
+                                    conn.sendall(b'Recieved: ' + data.decode())
                     print("Connection closed ..")                    
                     conn.close()
 
@@ -62,16 +84,23 @@ class SimpleServer:
         print("Server shutting down ...")
         sock.close()
     
+    def setClientSecret(self, client_msg):
+        """ Retrieve client secret from msg sent using public key """
+        self._client_secret = client_msg.split("~")[1]        
+
+    def getClientSecret(self):
+        """ Return client secret """        
+        return self._client_secret
 
     def getCert(self):
-        """ Create a mock certificate used by this server """
+        """ Return the mock certificate used by this server """
 
-        return "CA: I am Simple Server Certificate"
+        return self._server_certificate
 
     def getPublicKey(self):
         """ Create a simple string to represent the public key"""
 
-        return "Server Public Key"  
+        return self._server_public_key  
 
     def compileCertificate(self):
          """ Put together both the Certificate and Public Key """
