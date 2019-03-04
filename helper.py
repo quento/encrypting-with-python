6b6fdc288693f53e68ef738d7bc5be678a26b4ef
QuentO <qochieng@gmail.com>
2019-03-03 19:29:46 -0800

    Update helper.py
    Removed ceasarCipher. Created simpler simpleCipher() that does not require outside modules.

--- helper.py
+++ helper.py
@@ -1,20 +1,45 @@
-import string
-from string import ascii_lowercase
 
-def ceasarCipher(msg_txt, shift):
-    """ Simple Ceasar cipher used to illustrate key encryption. """
-    alphabet = string.ascii_lowercase
+def simpleCipher(msg, shift, key):
+    """ 
+        Simple ceasar cipher that scrambles/unscrambles a msg.
+        @msg = Message to encrypt.
+        @shift = How many characters to shift. integer.
+        @key = 'e' (any character) for Encrypt or 'd' for decrypt.
+    """
+    if key == 'd':
+        key = -shift
+    else:
+        key = shift
+
+    translated = ''
+
+    for symbol in msg:
+        if symbol.isalpha():
+            num = ord(symbol)
+            num += key
+
+            if symbol.isupper():
+                if num > ord('Z'):
+                    num -= 26
+                elif num < ord('A'):
+                    num += 26
+            elif symbol.islower():
+                if num > ord('z'):
+                    num -= 26
+                elif num < ord('a'):
+                    num += 26
+            
+            translated += chr(num)
+        else:
+            translated += symbol
     
-    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    
-    table = string.maketrans(alphabet, shifted_alphabet)
-    
-    return msg_txt.translate(table)
+    return translated
+        
 
 def readCert(cert,shift):
     """ Check server certificate. If valid, return true. If not return false """
     
-    server_cert = "I am Simple Server"
+    server_cert = "CA: I am Simple Server Certificate"
 
     decipher_cert = ceasarCipher(cert, -shift)
     
@@ -30,14 +55,26 @@ def readCert(cert,shift):
 
 def main():
     """ Test the helper functions """
-    server_cert = "I am Simple Server!"
-    shift = 1
-    encoded_cert = ceasarCipher(server_cert,shift)
+
+    print("*************** TEST simpleCipher() ***************")
+    certificate = "CA: I am Simple Server Certificate~Server Public Key"
+    cert_array = certificate.split("~")
+    server_cert = cert_array[0]
+    server_public_key = cert_array[1]
     
-    print("Msg to encode: ", server_cert)
-    print("Encoded Msg: ", ceasarCipher(server_cert,shift))
-    print("Decoded Msg: ", ceasarCipher(encoded_cert,-shift))
-    print('Return value of readCert()', readCert(encoded_cert,shift))
+    print( "Cert: = " + server_cert )
+    print("Encrypted = " + simpleCipher(server_cert,1,'e') )
+    print( "Decrypted = " + simpleCipher( simpleCipher(server_cert,1,'e') ,1,'d') )
+    
+    # print( "Cert: = " + server_cert )
+    # print( "Cert Public Key: " + server_public_key )
+    # shift = 1
+    # encoded_cert = ceasarCipher(server_cert,shift)
+    
+    # print( "Msg to encode: " + server_cert )
+    # print( "Encoded Msg: " + ceasarCipher(server_cert,shift) )
+    # print( "Decoded Msg: " + ceasarCipher(encoded_cert,-shift) )
+    # print( 'Return value of readCert(): ' + str(readCert(encoded_cert,shift)) )
 
 if __name__ == '__main__':
     main()
